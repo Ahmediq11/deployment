6455c9392a10fc9b5af62e5b2a38a01421579d54
unknown <ahmedhazm41@gmail.com>
2025-01-24 08:59:53 +0300

    add vercel.json

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.use("/", (req, res) => {
-  res.send("serveris running");
+  res.send("serveris  f running");
 });
 
 app.listen(5000, console.log("server is listening"));
